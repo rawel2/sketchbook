cdda7e5679260d1877c89a2582f962a415251471
rawel <rawel2@orange.pl>
2020-06-17 20:51:44 +0200

    Aktualizacja plus  remotexy

---
+++ project/project.ino
@@ -0,0 +1,77 @@
+/*
+   -- test --
+   
+   This source code of graphical user interface 
+   has been generated automatically by RemoteXY editor.
+   To compile this code using RemoteXY library 2.4.3 or later version 
+   download by link http://remotexy.com/en/library/
+   To connect using RemoteXY mobile app by link http://remotexy.com/en/download/                   
+     - for ANDROID 4.5.1 or later version;
+     - for iOS 1.4.1 or later version;
+    
+   This source code is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.    
+*/
+
+//////////////////////////////////////////////
+//        RemoteXY include library          //
+//////////////////////////////////////////////
+
+// RemoteXY select connection mode and include library 
+#define REMOTEXY_MODE__ESP8266WIFI_LIB_POINT
+#include <ESP8266WiFi.h>
+
+#include <RemoteXY.h>
+
+// RemoteXY connection settings 
+#define REMOTEXY_WIFI_SSID "szajs2"
+#define REMOTEXY_WIFI_PASSWORD "bdghjkptw"
+#define REMOTEXY_SERVER_PORT 6377
+
+
+// RemoteXY configurate  
+#pragma pack(push, 1)
+uint8_t RemoteXY_CONF[] =
+  { 255,0,0,1,0,11,0,10,24,1,
+  66,131,14,10,16,7,2,50 };
+  
+// this structure defines all the variables and events of your control interface 
+struct {
+
+    // output variables
+  int8_t temperatura; // =0..100 level position 
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0 
+
+} RemoteXY;
+#pragma pack(pop)
+
+/////////////////////////////////////////////
+//           END RemoteXY include          //
+/////////////////////////////////////////////
+
+
+
+void setup() 
+{
+  RemoteXY_Init (); 
+  
+  
+  // TODO you setup code
+  
+}
+
+void loop() 
+{ 
+  RemoteXY_Handler ();
+  
+  
+  // TODO you loop code
+  // use the RemoteXY structure for data transfer
+  // do not call delay() 
+
+
+}
